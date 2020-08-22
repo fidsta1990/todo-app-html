5b13def0db814cae2fb0084f7c99eb888e5bf67a
Fidel Collin <fidelcollin@icloud.com>
2020-08-23 00:04:14 +0100

    set up form and script file for button

---
+++ script/app.js
@@ -0,0 +1,7 @@
+const circle = document.querySelector(".circle");
+
+window.addEventListener("DOMContentLoaded", () => {
+  circle.addEventListener("click", () => {
+    console.log("create pop up");
+  });
+});
