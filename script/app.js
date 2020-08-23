3988759822c664597a01ab0581cd28f86610d862
Fidel Collin <fidelcollin@icloud.com>
2020-08-23 01:01:45 +0100

    add toggle for opening and closing task form

--- script/app.js
+++ script/app.js
@@ -1,7 +1,22 @@
 const circle = document.querySelector(".circle");
+const taskForm = document.querySelector(".task-form");
+const closeBtn = document.querySelector(".close-btn");
 
 window.addEventListener("DOMContentLoaded", () => {
-  circle.addEventListener("click", () => {
-    console.log("create pop up");
-  });
+  formOpen();
+  formClose();
 });
+
+function formOpen() {
+  circle.addEventListener("click", () => {
+    // console.log("create pop up");
+
+    taskForm.classList.add("active");
+  });
+}
+
+function formClose() {
+  closeBtn.addEventListener("click", () => {
+    taskForm.classList.remove("active");
+  });
+}
